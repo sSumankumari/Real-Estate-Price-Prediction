a2aa3cc82185d17c47afda5a99036c76539cd818
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2024-02-14 16:23:52 +0530

    var url modified

--- static/app.js
+++ static/app.js
@@ -28,7 +28,7 @@ function onClickedEstimatePrice() {
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
 
-    var url = "http://127.0.0.1:5000/predict_home_price";
+    var url = "/predict_home_price";
 
     // Validate inputs
     $.post(url, {
@@ -53,7 +53,7 @@ $("#btnSubmit").click(function () {
 
 function onPageLoad() {
     console.log("document loaded");
-    var url = "http://127.0.0.1:5000/get_location_names";
+    var url = "/get_location_names";
     
     $.get(url, function (data, status) {
         console.log("got response for get_location_names request");
