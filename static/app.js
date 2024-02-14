fbccb15c651acc3f252ead5037b3f18ac7260585
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2024-02-14 16:14:10 +0530

    var url updated

--- static/app.js
+++ static/app.js
@@ -29,7 +29,8 @@ function onClickedEstimatePrice() {
     var estPrice = document.getElementById("uiEstimatedPrice");
 
     var url = "http://127.0.0.1:5000/predict_home_price";
-    // var url = "/api/predict_home_price"; 
+
+    // Validate inputs
     $.post(url, {
         total_sqft: parseFloat(sqft.value),
         bhk: bhk,
@@ -53,7 +54,7 @@ $("#btnSubmit").click(function () {
 function onPageLoad() {
     console.log("document loaded");
     var url = "http://127.0.0.1:5000/get_location_names";
-    // var url = "/api/get_location_names";
+    
     $.get(url, function (data, status) {
         console.log("got response for get_location_names request");
         if (data && data.locations) {
